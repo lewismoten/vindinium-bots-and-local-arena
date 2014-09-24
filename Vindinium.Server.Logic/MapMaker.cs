b272c12748c4e48fb5fc56b3a4ad5e3b6fe8e15f
Lewis Moten <lewismoten@gmail.com>
2014-09-24 02:48:58 -0400

    #2 - start with obstructions, connect top map with bottom

--- Vindinium.Server.Logic/MapMaker.cs
+++ Vindinium.Server.Logic/MapMaker.cs
@@ -11,32 +11,13 @@ namespace Vindinium.Game.Logic
             var random = new Random(seed);
             int quarter = random.Next(5, 14);
             int size = quarter*2;
-            grid.MapText = "".PadLeft(size*size*2, ' ');
-            AddToken(grid, TokenHelper.Tavern, random);
-            AddToken(grid, TokenHelper.Player(0), random);
-            for (int i = 0; i < 4; i++)
-            {
-                AddToken(grid, TokenHelper.NeutralMine, random);
-            }
-            for (int i = 0; i < 8; i++)
-            {
-                AddToken(grid, TokenHelper.Obstruction, random);
-            }
+            grid.MapText = "".PadLeft(size*size*2, '#');
+
+            var pos = new Pos {Y = quarter, X = random.Next(1, quarter + 1)};
+            grid[pos] = TokenHelper.OpenPath;
 
             grid.MakeSymmetric();
             return grid.MapText;
-        }
-
-        private static void AddToken(Grid grid, string token, Random random)
-        {
-            int max = grid.Size/2 + 1;
-            var pos = new Pos {X = random.Next(1, max), Y = random.Next(1, max)};
-            while (grid[pos] != TokenHelper.OpenPath)
-            {
-                pos.X = random.Next(1, max);
-                pos.Y = random.Next(1, max);
-            }
-            grid[pos] = token;
         }
     }
 }

9b40f00f1357c886c5788f18fd3715ad9d05f8d5
Lewis Moten <lewismoten@gmail.com>
2014-09-27 00:37:58 -0400

    #2 - Refactor map maker tests

--- Vindinium.Game.Logic.Tests/MapMakerTests.cs
+++ Vindinium.Game.Logic.Tests/MapMakerTests.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
-using Vindinium.Common;
 using Vindinium.Common.DataStructures;
 using Vindinium.Game.Logic.Tests.Mocks;
 
@@ -15,18 +14,17 @@ namespace Vindinium.Game.Logic.Tests
         private const int MaxSeed = int.MaxValue;
         private const int MinSeed = int.MinValue;
         private readonly string[] _openTokens = {"  ", "@1", "@2", "@3", "@4"};
+        private IRandomizer _randomizer;
+        private IBoardHelper _boardHelper;
+        private IMapMaker _mapMaker;
+        private Board _board;
 
-        private static Dictionary<string, int> TokensOnNewMap(int seed)
+        private Dictionary<string, int> UniqueTokenCounts()
         {
-            var board = new Board();
-            IBoardHelper boardHelper = new BoardHelper(board);
-            var mapmaker = new MapMaker(new MockRandomizer(seed));
-            mapmaker.GenerateMap(boardHelper);
-
             var actualTokens = new Dictionary<string, int>();
-            boardHelper.ForEach(p =>
+            _boardHelper.ForEach(p =>
             {
-                string token = boardHelper[p];
+                string token = _boardHelper[p];
                 if (actualTokens.ContainsKey(token))
                 {
                     actualTokens[token]++;
@@ -39,17 +37,12 @@ namespace Vindinium.Game.Logic.Tests
             return actualTokens;
         }
 
-        private void AssertTokenIsAlwaysBesideAnother(int seed, string token, string[] acceptableNeighbors)
+        private void AssertTokenIsAlwaysBesideAnother(string token, string[] acceptableNeighbors)
         {
-            var board = new Board();
-            IBoardHelper boardHelper = new BoardHelper(board);
-            var mapmaker = new MapMaker(new MockRandomizer(seed));
-            mapmaker.GenerateMap(boardHelper);
-
-            boardHelper.ForEach(p =>
+            _boardHelper.ForEach(p =>
             {
-                if (boardHelper[p] != token) return;
-                AdjacentTokens adjacentTokens = boardHelper.GetAdjacentTokens(p);
+                if (_boardHelper[p] != token) return;
+                AdjacentTokens adjacentTokens = _boardHelper.GetAdjacentTokens(p);
                 var actualNeighbors = new[]
                 {
                     adjacentTokens.North.Token,
@@ -64,15 +57,26 @@ namespace Vindinium.Game.Logic.Tests
                     token,
                     p,
                     string.Join("', '", acceptableNeighbors),
-                    boardHelper
+                    _boardHelper
                     );
             });
+        }
+
+        private void GenerateMap(int seed)
+        {
+            _randomizer = new MockRandomizer(seed);
+            _board = new Board();
+            _boardHelper = new BoardHelper(_board);
+            _mapMaker = new MapMaker(_randomizer);
+            _mapMaker.GenerateMap(_boardHelper);
         }
 
         [Test]
         public void MapDoesNotHaveUnexpectedTokens([Random(MinSeed, MaxSeed, SeedCount)] int seed)
         {
-            string[] actualTokens = TokensOnNewMap(seed).Select(p => p.Key).ToArray();
+            GenerateMap(seed);
+
+            string[] actualTokens = UniqueTokenCounts().Select(p => p.Key).ToArray();
             var expectedTokens = new[] {"@1", "@2", "@3", "@4", "$1", "$2", "$3", "$4", "[]", "$-", "##", "  "};
             Assert.That(actualTokens, Is.SubsetOf(expectedTokens));
         }
@@ -80,54 +84,68 @@ namespace Vindinium.Game.Logic.Tests
         [Test]
         public void MapHasEmptyMines([Random(MinSeed, MaxSeed, SeedCount)] int seed)
         {
-            string[] actualTokens = TokensOnNewMap(seed).Select(p => p.Key).ToArray();
+            GenerateMap(seed);
+            string[] actualTokens = UniqueTokenCounts().Select(p => p.Key).ToArray();
             Assert.That(actualTokens, Has.Member("$-"));
-            Assert.That(actualTokens, Has.No.Member("$1"));
-            Assert.That(actualTokens, Has.No.Member("$2"));
-            Assert.That(actualTokens, Has.No.Member("$3"));
-            Assert.That(actualTokens, Has.No.Member("$4"));
         }
 
         [Test]
         public void MapHasEmptyPath([Random(MinSeed, MaxSeed, SeedCount)] int seed)
         {
-            Dictionary<string, int> actualTokens = TokensOnNewMap(seed);
+            GenerateMap(seed);
+            Dictionary<string, int> actualTokens = UniqueTokenCounts();
             Assert.That(actualTokens["  "], Is.AtLeast(1));
         }
 
         [Test]
         public void MapHasFourTaverns([Random(MinSeed, MaxSeed, SeedCount)] int seed)
         {
-            Dictionary<string, int> actualTokens = TokensOnNewMap(seed);
+            GenerateMap(seed);
+            Dictionary<string, int> actualTokens = UniqueTokenCounts();
             Assert.That(actualTokens["[]"], Is.EqualTo(4));
         }
 
         [Test]
         public void MapHasImpassibleWoods([Random(MinSeed, MaxSeed, SeedCount)] int seed)
         {
-            Dictionary<string, int> actualTokens = TokensOnNewMap(seed);
+            GenerateMap(seed);
+            Dictionary<string, int> actualTokens = UniqueTokenCounts();
             Assert.That(actualTokens["##"], Is.GreaterThan(0));
         }
 
         [Test]
-        public void MapHasPlayers([Random(MinSeed, MaxSeed, SeedCount)] int seed)
+        public void MapHasNoPlayerMines([Random(MinSeed, MaxSeed, SeedCount)] int seed)
         {
-            Dictionary<string, int> actualTokens = TokensOnNewMap(seed);
-            Assert.That(actualTokens["@1"], Is.EqualTo(1));
-            Assert.That(actualTokens["@2"], Is.EqualTo(1));
-            Assert.That(actualTokens["@3"], Is.EqualTo(1));
-            Assert.That(actualTokens["@4"], Is.EqualTo(1));
+            GenerateMap(seed);
+            string[] actualTokens = UniqueTokenCounts().Select(p => p.Key).ToArray();
+
+            Assert.That(actualTokens,
+                Has.No.Member("$1")
+                    .And.No.Member("$2")
+                    .And.No.Member("$3")
+                    .And.No.Member("$4")
+                );
         }
 
         [Test]
-        public void MapIsSymmetric([Random(MinSeed, MaxSeed, SeedCount)] int seed)
+        public void MapHasOneOfEachPlayer([Random(MinSeed, MaxSeed, SeedCount)] int seed)
         {
-            var board = new Board();
-            IBoardHelper boardHelper = new BoardHelper(board);
-            var mapmaker = new MapMaker(new MockRandomizer(seed));
-            mapmaker.GenerateMap(boardHelper);
+            GenerateMap(seed);
+            Dictionary<string, int> statistics = UniqueTokenCounts();
+            string[] tokens = statistics.Select(p => string.Format("{0} = {1}", p.Key, p.Value)).ToArray();
+            Assert.That(tokens,
+                Has.Member("@1 = 1")
+                    .And.Member("@2 = 1")
+                    .And.Member("@3 = 1")
+                    .And.Member("@4 = 1"));
+        }
 
-            string map = board.MapText.Replace("$-", "$$")
+        [Test]
+        public void MapMirrorsLeftAndRight([Random(MinSeed, MaxSeed, SeedCount)] int seed)
+        {
+            GenerateMap(seed);
+
+            string map = _board.MapText.Replace("$-", "$$")
                 .Replace("[]", "[[")
                 .Replace("@1", "@@")
                 .Replace("@2", "@@")
@@ -135,6 +153,19 @@ namespace Vindinium.Game.Logic.Tests
                 .Replace("@4", "@@");
 
             Assert.That(map, Is.EqualTo(map.Reverse()));
+        }
+
+        [Test]
+        public void MapMirrorsTopAndBottom([Random(MinSeed, MaxSeed, SeedCount)] int seed)
+        {
+            GenerateMap(seed);
+
+            string map = _board.MapText.Replace("$-", "$$")
+                .Replace("[]", "[[")
+                .Replace("@1", "@@")
+                .Replace("@2", "@@")
+                .Replace("@3", "@@")
+                .Replace("@4", "@@");
 
             int cells = map.Length/2;
             var size = (int) Math.Sqrt(cells);
@@ -150,51 +181,42 @@ namespace Vindinium.Game.Logic.Tests
         [Test]
         public void MapTextIsExpectedLength([Random(MinSeed, MaxSeed, SeedCount)] int seed)
         {
-            var board = new Board();
-            IBoardHelper boardHelper = new BoardHelper(board);
-            var mapmaker = new MapMaker(new MockRandomizer(seed));
-            mapmaker.GenerateMap(boardHelper);
-            string text = board.MapText;
+            GenerateMap(seed);
 
-            int cells = text.Length/2;
+            int cells = _board.MapText.Length/2;
             double size = Math.Sqrt(cells);
-            Assert.That(size, Is.EqualTo(Math.Floor(size)));
-            Assert.That(size, Is.AtLeast(10));
-            Assert.That(size, Is.AtMost(28));
+            Assert.That(size,
+                Is.EqualTo(Math.Floor(size))
+                    .And.InRange(10, 28));
             Assert.That(size%2, Is.Not.EqualTo(1));
         }
 
         [Test]
         public void MinesAreNextToOpenPath([Random(MinSeed, MaxSeed, SeedCount)] int seed)
         {
-            AssertTokenIsAlwaysBesideAnother(seed, "$-", _openTokens);
+            GenerateMap(seed);
+            AssertTokenIsAlwaysBesideAnother("$-", _openTokens);
         }
 
         [Test]
         public void OpenPathBetweenLeftAndRightQuadrant([Random(MinSeed, MaxSeed, SeedCount)] int seed)
         {
-            var board = new Board();
-            IBoardHelper boardHelper = new BoardHelper(board);
-            var mapmaker = new MapMaker(new MockRandomizer(seed));
-            mapmaker.GenerateMap(boardHelper);
+            GenerateMap(seed);
 
-            int half = boardHelper.Size/2;
+            int half = _boardHelper.Size/2;
             for (int y = 1; y <= half; y++)
             {
-                if (_openTokens.Any(t => t == boardHelper[half, y])) Assert.Pass();
+                if (_openTokens.Any(t => t == _boardHelper[half, y])) Assert.Pass();
             }
-            Assert.Fail("Missing path at top-left quardrent on right edge.\r\n{0}", board);
+            Assert.Fail("Missing path at top-left quardrent on right edge.\r\n{0}", _boardHelper);
         }
 
         [Test]
         public void OpenPathBetweenTopAndBottomQuadrant([Random(MinSeed, MaxSeed, SeedCount)] int seed)
         {
-            var board = new Board();
-            IBoardHelper boardHelper = new BoardHelper(board);
-            var mapmaker = new MapMaker(new MockRandomizer(seed));
-            mapmaker.GenerateMap(boardHelper);
+            GenerateMap(seed);
 
-            string tokens = board.MapText;
+            string tokens = _board.MapText;
             var size = (int) Math.Sqrt(tokens.Length/2.0);
 
             // first line of tokens in bottom left quadrant
@@ -202,28 +224,31 @@ namespace Vindinium.Game.Logic.Tests
 
             Assert.That(_openTokens.Any(t => borderTokens.IndexOf(t, StringComparison.Ordinal) != -1), Is.True,
                 "boardHelper does not have open path on quadrant border\r\n{0}",
-                MapFormatter.FormatTokensAsMap(tokens));
+                _boardHelper);
         }
 
         [Test]
         public void OpenPathIsNextToAnotherOpenPath([Random(MinSeed, MaxSeed, SeedCount)] int seed)
         {
-            AssertTokenIsAlwaysBesideAnother(seed, "  ", _openTokens);
+            GenerateMap(seed);
+            AssertTokenIsAlwaysBesideAnother("  ", _openTokens);
         }
 
         [Test]
         public void PlayersAreNextToOpenPath([Random(MinSeed, MaxSeed, SeedCount)] int seed)
         {
-            AssertTokenIsAlwaysBesideAnother(seed, "@1", _openTokens);
-            AssertTokenIsAlwaysBesideAnother(seed, "@2", _openTokens);
-            AssertTokenIsAlwaysBesideAnother(seed, "@3", _openTokens);
-            AssertTokenIsAlwaysBesideAnother(seed, "@4", _openTokens);
+            GenerateMap(seed);
+            AssertTokenIsAlwaysBesideAnother("@1", _openTokens);
+            AssertTokenIsAlwaysBesideAnother("@2", _openTokens);
+            AssertTokenIsAlwaysBesideAnother("@3", _openTokens);
+            AssertTokenIsAlwaysBesideAnother("@4", _openTokens);
         }
 
         [Test]
         public void TavernsAreNextToOpenPath([Random(MinSeed, MaxSeed, SeedCount)] int seed)
         {
-            AssertTokenIsAlwaysBesideAnother(seed, "[]", _openTokens);
+            GenerateMap(seed);
+            AssertTokenIsAlwaysBesideAnother("[]", _openTokens);
         }
     }
 }
